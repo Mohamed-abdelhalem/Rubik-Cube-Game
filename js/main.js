4b2467a6681d21f9f0efe7f4bb8dde6082278a27
Muhammed Halem <mohammed20191700553@cis.asu.edu.eg>
2022-04-25 21:06:20 +0200

    first publish

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let mm = document.getElementsByClassName('square_19');
+
+$('.square_39').click(function()
+   {
+    $('.square_19').fadeOut(10);
+   });
+   $('.square_39').click(function()
+      {
+       $('.square_22').fadeOut(10);
+      });
+      $('.square_39').click(function()
+         {
+          $('.square_25').fadeOut(10);
+         });
+
+
+
+         $('.square_42').click(function()
+            {
+             $('.square_20').fadeOut(10);
+            });
+            $('.square_42').click(function()
+               {
+                $('.square_23').fadeOut(10);
+               });
+               $('.square_42').click(function()
+                  {
+                   $('.square_26').fadeOut(10);
+                  });
+
+
+
+
+                  $('.square_45').click(function()
+                     {
+                      $('.square_21').fadeOut(10);
+                     });
+                     $('.square_45').click(function()
+                        {
+                         $('.square_24').fadeOut(10);
+                        });
+                        $('.square_45').click(function()
+                           {
+                            $('.square_27').fadeOut(10);
+                           });
